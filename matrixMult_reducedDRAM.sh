9b9b79f2a35177661ffede11055fcafdd199cc6b
Keegan Smith <keeganasmith@gmail.com>
2025-01-27 14:52:43 -0600

    working, but _very_ slow

--- matrixMult_reducedDRAM.sh
+++ matrixMult_reducedDRAM.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=fftw
-#SBATCH --time=02:00:00 --mem=480G
-#SBATCH --ntasks=1 --nodes=1 --cpus-per-task=9
+#SBATCH --time=12:00:00 --mem=480G
+#SBATCH --ntasks=1 --nodes=1 --cpus-per-task=96
 #SBATCH --output=out.log --error=error.log
-
+#SBATCH --partition=staff
 module purge
 module load GCC/13.2.0 FFTW/3.3.10
 
 cd /scratch/user/u.ks124812/wes_benchmarking/fft
 make fft
 
-for i in 1 2 4 8; do
-  watch "cat /proc/meminfo >> ./meminfo/meminfo_$i.log; date
-  +"%Y-%m-%dT%H:%M:%S%z" >> ./meminfo/meminfo_$i.log" &> /dev/null &
-  watch "ps -p $(pgrep -f "runner") -o %cpu,%mem,cmd --no-headers >>
-  ./ps_output/ps_output_$i.log; date +'%Y-%m-%dT%H:%M:%S%z' >> ./ps_output/ps_output_$i.log" &> /dev/null &
-  watch "free -h >> ./free_output/free_output_$i.log;  date +'%Y-%m-%dT%H:%M:%S%z' >>
-  ./free_output/free_outfree_output_$i.log" &> /dev/null &
-      echo Dimension 1: $i Dimension2: $i
+for i in 16 32 64 96; do
+  watch "cat /proc/meminfo >> ./meminfo_output/meminfo_$i.log; date+"%Y-%m-%dT%H:%M:%S%z" >> ./meminfo_output/meminfo_$i.log" &> /dev/null &
+  watch "ps -p $(pgrep -f "runner") -o %cpu,%mem,cmd --no-headers >>./ps_output/ps_output_$i.log; date +'%Y-%m-%dT%H:%M:%S%z' >> ./ps_output/ps_output_$i.log" &> /dev/null &
+  watch "free -h >> ./free_output/free_output_$i.log;  date +'%Y-%m-%dT%H:%M:%S%z' >>./free_output/free_output_$i.log" &> /dev/null &
+      echo num_threads: $i
       echo Start time:
       date
       ./runner -n $i
--- runner.cpp
+++ runner.cpp
@@ -4,11 +4,7 @@ using std::cout;
 int main(int argc, char* argv[]){
   int num_threads = 1;
 	long long NUM_DOUBLES = (long long)(INT32_MAX) * 2;
-  long long N = NUM_DOUBLES/1024;
-  cout << "num argc: " << argc << "\n";
-  if(argc > 2){
-    cout << argv[1] << "\n";
-  }
+  long long N = NUM_DOUBLES/32;
   if(argc > 2 && (strcmp(argv[1], "-n") == 0)){
     num_threads = stoi(argv[2]);
     compute_fft(N, num_threads);
