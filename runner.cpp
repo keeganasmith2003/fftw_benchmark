841c2055bfd1ecee9ce064bbad5bfc5439d39767
Keegan Smith <keeganasmith2003@gmail.com>
2025-01-27 12:17:22 -0600

    adjust number of doubles per thread

--- runner.cpp
+++ runner.cpp
@@ -2,16 +2,12 @@
 using std::stoi;
 using std::cout;
 int main(int argc, char* argv[]){
-	cout << "first arg was " << argv[1] << "\n";
-  int gigs = stoi(argv[1]);
   int num_threads = 1;
-  cout << "num gigs is " << (long long)gigs << "\n"; 
-	long long TOTAL_BYTES = (long long)gigs * 1024 * 1024 * 1024;
-	long long NUM_DOUBLES = TOTAL_BYTES / (sizeof(double));
+	long long NUM_DOUBLES = (long long)(INT32_MAX - 1) * 2;
   long long N = NUM_DOUBLES/2;
 	cout << "Amount of memory: " << gigs << " GB\n";
-  if(argc > 2 && argv[2] == "-n"){
-    num_threads = stoi(argv[3]);
+  if(argc > 2 && argv[1] == "-n"){
+    num_threads = stoi(argv[2]);
     compute_fft(N, num_threads);
   }
   else{
