e28454575839504416f8cdd050bfae13bad76075
Keegan Smith <keeganasmith@gmail.com>
2025-01-27 12:53:26 -0600

    openmp debugging

--- compute_fft.cpp
+++ compute_fft.cpp
@@ -5,32 +5,43 @@ using clock_type = chrono::high_resolution_clock;
   
 void compute_fft(long long n, int num_threads){
   omp_set_num_threads(num_threads);
-	fftw_complex *in, *out;
-	fftw_plan p;
-	cout << "size of each fft is: " << n << "\n";
+  cout << "size of each fft is: " << n << "\n";
   cout << "executing with " << num_threads << " threads\n";
   
   auto start = clock_type::now();
+  cout << "starting parallel section\n";
   #pragma omp parallel 
   {
+    fftw_complex *in, *out;
+    fftw_plan p; 
     in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * n);
     out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * n);
+    #pragma omp critical
+    { 
+      cout << "initialized in/out\n";
+    }
     populate_input(n, in);
-    cout << "segfault happened here2\n";
+    #pragma omp critical
+    {
+      cout << "populated input\n";
+    }
     p = fftw_plan_dft_1d(n, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-    cout << "segfault happened before execute, after plan\n";
+    #pragma omp critical
+    {
+      cout << "created plan\n";
+    }
     if (!in || !out) {
-      std::cerr << "Error: Memory allocation failed for FFT input/output arrays." << std::endl;
+      cerr << "FAILURE IN DATA ALLOCATION\n";
       exit(EXIT_FAILURE);
     }
     if (!p) {
-          std::cerr << "FFTW plan creation failed.\n";
+          cerr << "FFTW plan creation failed.\n";
           fftw_free(in);
           fftw_free(out);
           exit(EXIT_FAILURE);
       }
+    cout << "executing fft\n";
     fftw_execute(p);
-    cout << "seg fault happend here3\n";
     fftw_destroy_plan(p);
     fftw_free(in); fftw_free(out);
   }
--- runner.cpp
+++ runner.cpp
@@ -3,10 +3,13 @@ using std::stoi;
 using std::cout;
 int main(int argc, char* argv[]){
   int num_threads = 1;
-	long long NUM_DOUBLES = (long long)(INT32_MAX - 1) * 2;
-  long long N = NUM_DOUBLES/2;
-	cout << "Amount of memory: " << gigs << " GB\n";
-  if(argc > 2 && argv[1] == "-n"){
+	long long NUM_DOUBLES = (long long)(INT32_MAX) * 2;
+  long long N = NUM_DOUBLES/1024;
+  cout << "num argc: " << argc << "\n";
+  if(argc > 2){
+    cout << argv[1] << "\n";
+  }
+  if(argc > 2 && (strcmp(argv[1], "-n") == 0)){
     num_threads = stoi(argv[2]);
     compute_fft(N, num_threads);
   }
